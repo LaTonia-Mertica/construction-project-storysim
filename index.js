eeab5fa38a7fceb4261a2af632169bda9378dbb2
Sheppard Walker <latoniamertica@gmail.com>
2023-11-12 15:20:04 -0500

    3d map: added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+/*
+MAPPING
+*/
+new maplibregl.Map({
+  container: "map",
+  style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+  center: [-76.13928, 43.0352339],
+  zoom: 12,
+});
+
+/*
+SUBPAGES
+*/
 function showSubpage(pageId) {
   let page = document.getElementById(pageId);
   page.classList.add("active");
