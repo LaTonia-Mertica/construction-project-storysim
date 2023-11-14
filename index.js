346ac427fb3b5c9b9819414ee78a61c2256d7ac3
Sheppard Walker <latoniamertica@gmail.com>
2023-11-14 15:13:08 -0500

    map: condensed

--- index.js
+++ index.js
@@ -2,12 +2,28 @@
 MAPPING
 COPYRIGHT CARTO, COPYRIGHT OpenStreetMap contributors
 */
-new maplibregl.Map({
+const map = new maplibregl.Map({
   container: "map",
   style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
   center: [-75.4057828769, 43.225158266],
   zoom: 15,
 });
+
+// TOGGLE THEME FN
+function toggleTheme() {
+  let element = document.body;
+  element.classList.toggle("toggle-theme");
+
+  if (element.classList.contains("toggle-theme")) {
+    map.setStyle(
+      "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json"
+    );
+  } else {
+    map.setStyle(
+      "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
+    );
+  }
+}
 
 /*
 SUBPAGES
