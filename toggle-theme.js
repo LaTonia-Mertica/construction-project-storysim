3f9aa54bd3f2284f3dd059da826902d868a5c495
Sheppard Walker <latoniamertica@gmail.com>
2023-11-14 14:36:41 -0500

    toggle: toggles / use external styles (not fn)

--- toggle-theme.js
+++ toggle-theme.js
@@ -2,18 +2,19 @@ function toggleTheme() {
   let element = document.body;
   element.classList.toggle("toggle-theme");
 
-  new maplibregl.Map({
-    container: "map",
-    style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json",
-    center: [-75.4057828769, 43.225158266],
-    zoom: 15,
-  });
-
-  document.body.style.backgroundColor = "#1A170F";
-  document.body.querySelector("i.toggleBtn.fa-dumpster").style.color =
-    " #1A170F";
-  document.body.style.color = "#FFF";
-  document.body.querySelector("h1").style.color = "#EEE5D8";
-  document.body.querySelector("span").style.textShadow = ".025em 0 0 #FFF";
-  document.body.querySelector("button").style.backgroundColor = "#FFF";
+  if (element.classList.contains("toggle-theme")) {
+    new maplibregl.Map({
+      container: "map",
+      style: "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json",
+      center: [-75.4057828769, 43.225158266],
+      zoom: 15,
+    });
+  } else {
+    new maplibregl.Map({
+      container: "map",
+      style: "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json",
+      center: [-75.4057828769, 43.225158266],
+      zoom: 15,
+    });
+  }
 }
